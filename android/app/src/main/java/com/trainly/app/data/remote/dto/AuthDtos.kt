9a12ab9ac11c823b65374d9c3954684965b33e6e
“horacenjoroge” <125595726+horacenjorge@users.noreply.github.com>
2026-05-15 05:00:54 +0300

    feat: Add login/register/refresh DTOs

---
+++ android/app/src/main/java/com/trainly/app/data/remote/dto/AuthDtos.kt
@@ -0,0 +1,10 @@
+package com.trainly.app.data.remote.dto
+import com.google.gson.annotations.SerializedName
+
+data class LoginRequest(val email: String, val password: String)
+data class RegisterRequest(val name: String, val email: String, val password: String)
+data class AuthResponse(val token: String?, val refreshToken: String?, val user: UserDto?)
+data class RefreshTokenRequest(val refreshToken: String)
+data class RefreshTokenResponse(val token: String?, val refreshToken: String?)
+data class UserDto(@SerializedName("_id") val id: String?, val name: String?, val email: String?, val avatar: String?, val bio: String?, val location: String?, val stats: UserStatsDto?)
+data class UserStatsDto(val totalWorkouts: Int?, val totalDistance: Double?, val totalDuration: Int?, val totalCalories: Int?)
